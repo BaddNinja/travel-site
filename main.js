189bf01c0a58d0711eae64c63df572303319f6c1
Steven Whyte <baddninja2@gmail.com>
2017-11-02 16:05:37 -0700

    Updated Ajax, added Boarding Pass event handler

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
-	$('.confirmation').on('click', 'button', function() {
-		$(this).closest('.confirmation').find('.ticket').slideToggle();
+	$('.confirmation').on('click', 'button', function(e) {
+		var ticket = $(e.target).closest('.confirmation').find('.ticket'),
+			trip = ticket.data('trip');
+		$.ajax('https://BaddNinja.github.io/travel-site/html/' + trip + '.html', {
+			success: function(response) {
+				$(ticket).html(response).slideDown();
+			},
+			error: function(request, errorType, errorMessage) {
+				alert('Error: ' + errorType + ' with message: ' + errorMessage);
+			},
+			timeout: 3000
+		});
 	});
 
-	$('.confirmation').on('click', '.view-pass', function() {
+	$('.confirmation').on('click', '.view-pass', function(event) {
+		event.preventDefault();
 		$(this).closest('.ticket').find('.details').show();
 	});
 });
