70da1afd0bc00aad10d3fb25a795dfda4ca82156
Steven Whyte <baddninja2@gmail.com>
2017-11-10 16:10:40 -0800

    Trying to get JSON working

--- main2.js
+++ main2.js
@@ -5,21 +5,23 @@ var confirmation = {
 	},
 	loadConfirmation: function() {
 		var ticket = $(this).closest('.confirmation').find('.ticket'),
-			trip = ticket.data('trip');
-		$.ajax('https://BaddNinja.github.io/travel-site/data/' + trip + '.json', {
-			success: function(response) {
-				var msg;
-				msg.append('<p><strong>Destination: ' + '</strong>'+ response[0].location + '</p>');
-				msg.append('<p><strong>Departure: ' + '</strong>'+ response[0].departure + '</p>');
-				msg.append('<p><strong>Flight Number: ' + '</strong>'+ response[0].flight + '</p>');
-				msg.append('<p><strong>Boarding Pass: </strong><a class="view-pass" href="#">View Boarding Pass');
-				msg.append('</a><div class="details">boarding pass image HAHA!</div></p>');
-			$(ticket).html(msg).slideDown();
-			},
-			error: function(request, errorType, errorMessage) {
-				alert('Error: ' + errorType + ' with message: ' + errorMessage);
-			},
-			timeout: 3000
+			url = 'http://www.filltext.com/?callback=?';
+		$.getJSON(url, {
+			'rows': 3,
+			'country': '{country}',
+			'departure': '{date|01-01-2018,01-15-2018}',
+			'flight': '{numberRange|1000to4999}'
+		})
+		.done(function( data ) {
+			$.each(data, function(i, item) {
+				var msg =
+					'<p><strong>Destination: ' + '</strong>'+ item.country + '</p>' +
+					'<p><strong>Departure: ' + '</strong>'+ item.departure + '</p>' +
+					'<p><strong>Flight Number: ' + '</strong>'+ item.flight + '</p>' +
+					'<p><strong>Boarding Pass: </strong><a class="view-pass" href="#">View Boarding Pass' +
+					'</a><div class="details">boarding pass image</div></p>';
+				$(ticket).html(msg).slideDown();
+			});
 		});
 	},
 	showBoardingPass: function() {
---
+++ obj.js
@@ -0,0 +1,31 @@
+var confirmation = {
+	init: function() {
+		$('.confirmation').on('click', 'button', this.loadConfirmation);
+		$('.confirmation').on('click', '.view-pass', this.showBoardingPass);
+	},
+	loadConfirmation: function() {
+		var ticket = $(this).closest('.confirmation').find('.ticket'),
+			trip = ticket.data('trip');
+		$.ajax('https://BaddNinja.github.io/travel-site/data/' + trip + '.json', {
+			dataType: 'json',
+			contentType: 'application/json',
+			success: function(response) {
+				alert(response);
+				//$(ticket).html(response).slideDown();
+			},
+			error: function(request, errorType, errorMessage) {
+				alert('Error: ' + errorType + ' with message: ' + errorMessage);
+			},
+			timeout: 3000
+		});
+	},
+	showBoardingPass: function() {
+		event.preventDefault();
+		$(this).closest('.ticket').find('.details').show();
+		$(this).hide();
+	}
+};
+
+$(document).ready(function() {
+	confirmation.init();
+});
